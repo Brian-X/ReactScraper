5c983dac1dad7ee113b8cff2cda3145fb40ef29f
Brian-X <brian@bae-media.com>
2017-10-17 22:21:27 -0500

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import Books from "./pages/Books";
+import Main from "./pages/Main";
 import Nav from "./components/Nav";
 
 const App = () => 
   <div>
     <Nav />
-    <Books />
+    <Main />
   </div>;
 
 export default App;
